3c3046528c0775a186c621eefa27e74281ba0bab
uttkxrrsh <uttkarsh20479@iiitd.ac.in>
2022-01-14 22:30:33 +0530

    commit

--- palindromereorder.cpp
+++ palindromereorder.cpp
@@ -10,6 +10,12 @@ using namespace std;
 
 int main(){
     string s;
-    
+    unordered_set<char, int> map;
+    for(int i =0; i<26; i++){
+        map.insert(itoa(i),1);
+    }
+    for(auto i: map){
+        cout<<i<<"\n";
+    }
     return 0;
 }
