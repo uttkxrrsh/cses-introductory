5fa824fc2d2e08e8b2f22daac9c37eb7fdff57d4
uttkxrrsh <uttkarsh20479@iiitd.ac.in>
2022-06-09 21:13:38 +0530

    Made changes to palindromereorder

--- palindromereorder.cpp
+++ palindromereorder.cpp
@@ -10,12 +10,22 @@ using namespace std;
 
 int main(){
     string s;
-    unordered_map<char, int> map;
+    unordered_map<string, int> map;
+    int charcount, oddcount;
+    vector<string> v1,v2;
+    cin>>s;
     for(int i =0; i<26; i++){
-        
+        charcount = count(s.begin(), s.end(), (char)(65+i));
+        if(charcount > 0){
+            map[to_string(65+i)] = charcount;
+            if(charcount%2 == 1)
+                oddcount++;
+        }
     }
-    for(auto i: map){
-        cout<<i.s<<"\n";
+    if(oddcount>=1 && s.length()%2==0){
+        cout<<"NO SOLUTION";
+        return 0;
     }
+
     return 0;
 }
