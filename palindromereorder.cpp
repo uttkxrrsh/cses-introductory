edc565e1d29972a8cf5696194ccb5cf28569b334
uttkxrrsh <uttkarsh20479@iiitd.ac.in>
2022-06-09 23:11:54 +0530

    palindromeorder.cpp

--- palindromereorder.cpp
+++ palindromereorder.cpp
@@ -9,23 +9,31 @@
 using namespace std;
 
 int main(){
-    string s;
-    unordered_map<string, int> map;
-    int charcount, oddcount;
-    vector<string> v1,v2;
-    cin>>s;
+    string s0,s1 = "",s2 = "";
+    char oddchar;
+    unordered_map<char, int> map;
+    int charcount, oddcount = 0;
+    cin>>s0;
     for(int i =0; i<26; i++){
-        charcount = count(s.begin(), s.end(), (char)(65+i));
+        charcount = count(s0.begin(), s0.end(), (char)(65+i));
         if(charcount > 0){
-            map[to_string(65+i)] = charcount;
-            if(charcount%2 == 1)
+            map[(char)(65+i)] = charcount;
+            if(charcount%2 == 1){
                 oddcount++;
+                oddchar = char(65+i);
+            }
         }
     }
-    if(oddcount>=1 && s.length()%2==0){
+    if((oddcount>=1 && s0.length()%2==0) || (oddcount == s0.length() && s0.length()>1)){
         cout<<"NO SOLUTION";
         return 0;
     }
-
+    for(auto i:map){
+        string sbuild(i.s/2, i.f);
+        s1 = s1 + sbuild;
+        s2 = sbuild + s2;
+    }
+    string ans = (oddcount == 1) ? s1 + oddchar + s2 : s1 + s2;
+    cout <<ans;
     return 0;
 }
