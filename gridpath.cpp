a3a2a5b5745239f033b781ab9fca9c20690e50d6
uttkxrrsh <uttkarsh20479@iiitd.ac.in>
2023-03-10 20:00:49 +0530

    gridpath changes

--- gridpath.cpp
+++ gridpath.cpp
@@ -16,7 +16,7 @@
 #define FAST ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
 using namespace std;
 
-getpath()
+vector<vector<bool>> visited(7, vector<bool> (7,false));
 
 int main(){
     FAST
@@ -24,4 +24,36 @@ int main(){
     getline(cin,path);
 
     return 0;
+}
+
+//cses problem grid path
+ int getpath(int r, int c, int path, int &count){
+
+    `
+    if(r==6 && c == 6){
+        count++;
+    }
+
+    visited[r][c] = true;
+    if(r<6 && !visited[r+1][c]){
+        path++;
+        getpath(r+1,c,path);
+        path--;
+    }
+    if(r>0 && !visited[r-1][c]){
+        path++;
+        getpath(r-1,c,path);
+        path--;
+    }
+    if(c<6 && !visited[r][c+1]){
+        path++;
+        getpath(r,c+1,path);
+        path--;
+    }
+    if(c>0 && !visited[r-1[c]]){
+        path++;
+        getpath(r-1,c,path);
+        path--;
+    }
+    visited[r][c] = false;
 }
